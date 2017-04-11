3b76942c4a67256d93cbeedb1dd03f8fdc95fc2b
Bailey Kursar <bkursar@gmail.com>
2017-04-11 17:00:21 +0100

    Saving progress, we're on point 10 of the challenge

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,6 +2,10 @@ require 'docking_station'
 
 describe DockingStation do
   describe ".release_bike" do
+    ## This next line is the one that's not right
+    it { should eq Bike.new }
+  end
+  describe ".working?" do
     it { should respond_to }
   end
 end
