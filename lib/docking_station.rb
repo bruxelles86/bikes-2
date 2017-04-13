bdc4c62d5f7394b58f740fcfd861d4521ab2f554
bruxelles86 <w_gant@yahoo.co.uk>
2017-04-13 14:17:31 +0100

    Beginning step 14

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -3,10 +3,11 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
-    Bike.new
+    fail "There are no bikes" unless @bike
   end
 
   def dock(bike)
+    fail "The station is full" if @bike
     @bike = bike
   end
 
--- spec/boris-bikes_spec.rb
+++ spec/boris-bikes_spec.rb
@@ -5,21 +5,20 @@ describe DockingStation do
   ## The class 'DockingStation' should respond to the command to release a bike, using release_bike
   it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
+#  it 'releases working bikes' do
     ## (potential confusion) create a new variable 'bike' that is equal
     ## to the return value of 'release_bike' method on the new instance of the
     ## original class
     ## e.g. bike = DockingStation.new.release_bike
-    bike = subject.release_bike
+#    bike = subject.release_bike
     ## we should expect that the variable 'bike' should return 'true' when
     ## we call the method 'be_working' on it
-    expect(bike.working?).to(eq(true))
-  end
+#    expect(bike.working?).to(eq(true))
+#  end
 
 it { is_expected.to respond_to(:dock).with(1).argument}
 
 it { is_expected.to respond_to(:bike) }
-
 
 it 'returns docked bikes' do
   bike = Bike.new
@@ -28,6 +27,18 @@ it 'returns docked bikes' do
   expect(subject.bike).to eq bike
 end
 
+describe '#release_bike' do
+  it 'returns an error when there are no bikes' do
+    expect { subject.release_bike }.to raise_error "There are no bikes"
+  end
+end
+
+describe '#dock' do
+  it 'returns an error when station is full' do
+    subject.dock(Bike.new)
+    expect { subject.dock(Bike.new) }.to raise_error "The station is full"
+  end
+end
 #  it 'docks a new bike' do
 #    expect(subject.instance_variable_get(:@bike)).to be_an_instance_of(Bike)
 #  end
